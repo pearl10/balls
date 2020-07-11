338140068510edbd6cdb8fb63e690f8d23aee52e
pearl10 <67584957+pearl10@users.noreply.github.com>
2020-07-11 13:07:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,33 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var ball;
+var binLog1,log2,log3;
+var ground,;
+function preload(){
 
-function preload()
-{
 	
 }
 
 function setup() {
 	createCanvas(800, 700);
 
-
+  
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-
+	ball=new Ball(100,675);
+	binLog1=new Bin(500,660,20,250,20);
+	Log2=new Bin(650,660,20,250,20);
+	Log3=new Bin(575,660,180,20,20);
 
 	Engine.run(engine);
-  
+	
+	
+    ground=new Ground(400,680,800,40);
 }
 
 
@@ -30,7 +36,27 @@ function draw() {
   background(0);
   
   drawSprites();
- 
+ ball.display();
+ binLog1.display();
+ Log2.display();
+ Log3.display();
+ ground.display();
+}
+
+function keyPressed(){
+if(keyCode===UP_ARROW){
+
+Matter.Body.applyForce(ball.body,ball.body.position,{x:75,y:-75});
+
+
+}
+
+
+
+
+
+
+
 }
 
 
